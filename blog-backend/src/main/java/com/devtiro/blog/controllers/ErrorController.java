3dbfcaf74ecc22afb23020cef246f51f3a53a861
Andriy <karpus.andrii@proton.me>
2025-02-24 21:47:00 +0100

    Loading Users

--- blog-backend/src/main/java/com/devtiro/blog/controllers/ErrorController.java
+++ blog-backend/src/main/java/com/devtiro/blog/controllers/ErrorController.java
@@ -4,6 +4,7 @@ import com.devtiro.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +43,15 @@ public class ErrorController {
                 .build();
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect username or password")
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
